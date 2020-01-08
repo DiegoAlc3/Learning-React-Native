3181efcf3532ac2a637469f3e3e0d2c7634d9c47
Diego Alcantara <diegosalc15@gmail.com>
2020-01-08 11:12:00 -0300

    Getting API's data using Axios

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,38 @@
 import React,{Component} from 'react'
 import {View,Text} from 'react-native';
 
+import Api from '../services/api';
+import api from '../services/api';
+
 export default class Main extends Component{
   static navigationOptions={
     title:"JSHunt",
+  }
+
+  state = {
+    docs:[],
+  }
+
+  componentDidMount(){
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await api.get(`/products`);
+
+    const {docs } = response.data;
+
+    console.log(docs);
+    this.setState({ docs })
   }
     
   render(){
     return(
       <View>
-        <Text>Paloma da o cu </Text>
+        <Text>Produtos</Text>
+        {this.state.docs.map(product => {
+          return <Text key={product._id}>{product.title}</Text>
+        } )}
       </View>
     )
   }
