0d2f250db7742a7c133dba91b6249e7ffde1bafc
Diego Alcantara <diegosalc15@gmail.com>
2020-01-08 11:41:36 -0300

    Changing Function Map to FlatList and Styling the List

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import {View,Text} from 'react-native';
+import {View,Text,FlatList,TouchableOpacity,StyleSheet} from 'react-native';
 
 import Api from '../services/api';
 import api from '../services/api';
@@ -25,15 +25,76 @@ export default class Main extends Component{
     console.log(docs);
     this.setState({ docs })
   }
+  renderItem = ({item}) =>{
+    return(
+      <View style={styles.productContainer}>
+        <Text style={styles.productTitle} >{item.title}</Text>
+        <Text style={styles.productDescription}>{item.description}</Text>
+        <TouchableOpacity style={styles.productButton} onPress={()=>{}}>
+            <Text style={styles.buttonText}>Acessar </Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
     
   render(){
     return(
-      <View>
-        <Text>Produtos</Text>
-        {this.state.docs.map(product => {
-          return <Text key={product._id}>{product.title}</Text>
-        } )}
+      <View style={styles.container}> 
+        <FlatList 
+        contentContainerStyle={styles.list}
+        data={this.state.docs}
+        keyExtractor={item => item._id}
+        renderItem={this.renderItem}/>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#fafafa",
+  },
+
+  list:{
+    padding:20,
+  },
+
+  productContainer:{
+    backgroundColor:"#FFF",
+    borderWidth:1,
+    borderColor:"#DDD",
+    borderRadius:5,
+    padding:20,
+    marginBottom:20,
+  },
+
+  productTitle:{
+    fontSize:18,
+    fontWeight:"bold",
+    color:"#333",
+  },
+
+  productDescription:{
+    fontSize:16,
+    color:"#999",
+    marginTop:5,
+    lineHeight:24,
+  },
+
+  productButton:{
+    height:42,
+    borderRadius:5,
+    borderWidth:2,
+    borderColor:"#DA552F",
+    marginTop:10,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  buttonText:{
+    fontSize:16,
+    color:"#DA552F",
+    fontWeight:"bold",
+  },
+
+})
