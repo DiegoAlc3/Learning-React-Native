a0c36f6a8828398922c834383fec08efc8f59405
Diego Alcantara <diegosalc15@gmail.com>
2020-01-08 10:28:10 -0300

    Styling StatusBar

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,10 +2,14 @@ import React,{Component} from 'react'
 import {View,Text} from 'react-native';
 
 export default class Main extends Component{
+  static navigationOptions={
+    title:"JSHunt",
+  }
+    
   render(){
     return(
       <View>
-        <Text>Página Main</Text>
+        <Text>Hello World</Text>
       </View>
     )
   }
